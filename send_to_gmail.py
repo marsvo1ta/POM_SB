ebfb460a5673800c852ecf8d56cab56df8362f52
marsvo1ta <euromobilco@gmail.com>
2022-12-27 12:25:39 +0200

    send to email

---
+++ send_to_gmail.py
@@ -0,0 +1,17 @@
+import os
+from datetime import datetime as d
+import time
+
+from bs4 import BeautifulSoup
+from send_gmail import SendGMail
+
+
+txt = 'Pytest Result'
+subject = 'Pytest Result '
+date = d.utcnow().strftime("%A, %d. %B %Y %I:%M%p")
+
+smtpsrv = SendGMail(id=os.environ.get('RECEIVE_FROM_GMAIL'),pwd=os.environ.get('GMAIL_PYTEST_CODE'))
+with open('dashboard.html', 'r',) as report:
+    S = BeautifulSoup(report,features="html.parser")
+    smtpsrv.send(None, os.environ.get('SEND_TO_GMAIL'), subject=subject+date, text=txt, html=S)
+print(date)

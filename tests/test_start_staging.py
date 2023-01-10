f89d53801346371880cf9c909ae80d10ebdad996
marsvo1ta <euromobilco@gmail.com>
2023-01-10 12:11:37 +0200

    staging test

---
+++ tests/test_start_staging.py
@@ -0,0 +1,12 @@
+from seleniumbase import BaseCase
+
+from data.locators import *
+
+
+class TestNP(BaseCase):
+
+    def test_start_staging(self):
+        if self.is_element_present(START_DEPLOY_BUTTON):
+            self.click(START_DEPLOY_BUTTON)
+            self.assert_elements_visible(DEPLOY_LOADER)
+        

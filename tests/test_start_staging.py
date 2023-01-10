81735cf4ce9d132853d04b56efafdab7e14b227a
marsvo1ta <euromobilco@gmail.com>
2023-01-10 14:58:00 +0200

    add open link

--- tests/test_start_staging.py
+++ tests/test_start_staging.py
@@ -6,6 +6,7 @@ from data.locators import *
 class TestNP(BaseCase):
 
     def test_start_staging(self):
+        self.open(MAIN_URL)
         if self.is_element_present(START_DEPLOY_BUTTON):
             self.click(START_DEPLOY_BUTTON)
             self.assert_elements_visible(DEPLOY_LOADER)

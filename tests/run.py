cdc0bea2e442348593988c64398daac86872d1f5
marsvo1ta <euromobilco@gmail.com>
2023-04-05 17:12:45 +0300

    add args to run file

--- tests/run.py
+++ tests/run.py
@@ -1,11 +1,7 @@
 import pytest
-import datetime
 
 
 if __name__ == "__main__":
     
-    # pytest.main(['tests/test_0_start_staging.py', 'tests/test_6_calculator.py', 'tests/test_1_np.py', 'tests/test_1_register.py', "-s", "-v", "-n=4"])
-    start = datetime.datetime.today()
-    pytest.main(["-s", "-v", "-n=4", "--headless"])
-    end = datetime.datetime.today()
-    print(end-start)
+    pytest.main(["-s", "-v", "-n=4", "--headless", "--github-report", "--dashboard", "--pls=none", "--sjw"])
+    
